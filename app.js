75796cb83d319747637b909490616ec95ef0e97e
Pieter Michels <pieter@noort.be>
2011-03-30 14:26:48 +0200

    updates for rapid msg sending

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ var net = require('net')
 var global = 0;
 var server;
 var port = 8125;
+var msg_limiter = '~|~';
+var cmd_limiter = '~%~';
 
 // Init
 function run() {
-	//setup_db('127.0.0.1', 27017, 'logr');
+	// setup_db('127.0.0.1', 27017, 'logr');
 	setup_db('prox_logr:prox_logr@flame.mongohq.com', 27096, 'logr');
 
 	server.listen(port); 
@@ -37,12 +39,27 @@ function setup_db(host, port, db_name) {
 		,	msg 		: String
 		, type		: String
 		, meta		: String 
+		, project	: String
+		, uid			: String
 		,	date 		: {type: Date, default: Date.now}
 	});
 
 	mongoose.model('LogrMessage', LogrMessage);
 
 	LogrMessageModel = mongoose.model('LogrMessage');
+}
+
+function save_log(data, call_back) {
+	var obj = JSON.parse(data);
+	var log = new LogrMessageModel();
+
+	log.msg 		= obj.msg;
+	log.type 		= obj.type;
+	log.meta 		= obj.meta;
+	log.project	= obj.project;
+	log.uid 		= obj.uid;
+
+	log.save(call_back);
 }
 
 // Server
@@ -62,23 +79,25 @@ server = net.createServer(function (socket) {
 		
 		console.log('# New Client ' + socket.remoteAddress + ':' + socket.remotePort);
 		
-		sys.puts(sys.inspect(socket, false)); // Debug info
+		// sys.puts(sys.inspect(socket, false)); // Debug info
 	});
 	  
 	socket.on('data', function (data) {
-		var obj = JSON.parse(data);
-		var msg = new LogrMessageModel();
+		var messages = data.split(msg_limiter);
 		
-		msg.msg 	= obj.msg;
-		msg.type 	= obj.type;
-		msg.meta 	= obj.meta;
-		
-		msg.save(function(err){
-			if(err)
-				console.dir(err);
-		});
-		
-		socket.write(msg.date + ': ' + msg.id + ' :' + msg.message + '\0');
+		while(messages.length > 0) {
+			var msg = messages.shift();
+			
+			if(msg.length > 0) {
+				save_log(msg, function(err) {
+					if(err) {
+						console.log('# Data Error ' + sys.inspect(err, false));
+
+						socket.write(sys.inspect(err, false) + '\0');
+					}
+				});
+			}
+		}
 	});
 	
 	socket.on('end', function () {
