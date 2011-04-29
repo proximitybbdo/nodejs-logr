7f66c858e7149c71c0fba1f61f25163c85fc398a
Pieter Michels <pieter@noort.be>
2011-04-29 17:12:43 +0200

    adding realtime listening function

--- socketserver/lib/socket_io.js
+++ socketserver/lib/socket_io.js
@@ -11,7 +11,7 @@ SocketIO.prototype = {
   port: null, 
   server: null, 
   socket: null, 
-  client: null, 
+  clients: [], 
   is_verbose: false, 
 
   _init: function(port) {
@@ -21,6 +21,8 @@ SocketIO.prototype = {
   }, 
 
   start: function() {
+    var $this = this;
+
     this.server = http.createServer(function(req, res) { 
       res.writeHead(200, {'Content-Type': 'text/html'}); 
       res.end('logr-server');
@@ -29,33 +31,78 @@ SocketIO.prototype = {
     this.server.listen(this.port);
       
     this.socket = io.listen(this.server);
-
-    var $this = this;
     
     this.socket.on('connection', function(client){ 
-      if(this.is_verbose)
-        console.log("SIO :: Client connected"); 
-      
-      $this.client = client;
-
-      client.on('message', function(){
-        if(this.is_verbose)
-          console.log("SIO :: Client message"); 
-      }); 
-      
-      client.on('disconnect', function(){
-        if(this.is_verbose)
-          console.log("SIO :: Client disconnected"); 
-      }); 
+      $this._client_handler(client);
     });
-  }, 
+  },
 
   push: function(data) {
     if(this.is_verbose)
-      console.log("SIO :: Push (" + this.client + "): " + data);
+      console.log("SIO :: Push :: " + data);
 
-    if(this.client)
-      this.client.send({raw: data});
+    if(this.clients.length > 0) {
+      var listening_clients = this._get_listening_clients();
+      
+      for(var i in listening_clients)
+        listening_clients[i].conn.send({raw: data});
+    }
+  },
+
+  _client_handler: function(client) {
+    var $this = this;
+    var new_client = client;
+
+    if(this.is_verbose)
+      console.log("SIO :: Client connected (" + new_client.sessionId + ")"); 
+     
+    this.clients.push({conn: new_client, listening: false, filter: ''});
+
+    new_client.on('message', function(data) {
+      if($this.is_verbose)
+        console.log("SIO :: Client message (" + this.sessionId + ") -> (" + data + ")");
+
+      if(data.func == 'listen' && data.filter.length > 0) {
+        if($this.is_verbose)
+          console.log("SIO :: Client listening (" + this.sessionId + ")");
+
+        $this._get_client(this.sessionId).listening = true;
+        $this._get_client(this.sessionId).filter = data.filter;
+      }
+    }); 
+      
+    new_client.on('disconnect', function() {
+      if($this.is_verbose)
+        console.log("SIO :: Client disconnected (" + this.sessionId + ")"); 
+      
+      $this._remove_client(this.sessionId);
+    }); 
+  }, 
+
+  _get_listening_clients: function() {
+    return this.clients.filter(function(el, index, array) {
+      if(el.listening && el.filter.length > 0)
+        return true;
+      else
+        return false;
+    });                      
+  },
+
+  _get_client: function(sessionId) {
+    for(var i = 0; i < this.clients.length; i++) {
+      if(this.clients[i].conn.sessionId == sessionId)
+        return this.clients[i];
+    }
+  }, 
+
+  _remove_client: function(sessionId) {
+    for(var i = 0; i < this.clients.length; i++) {
+      if(this.clients[i].conn.sessionId == sessionId) {
+        this.clients.splice(i, 1);
+
+        break;
+      }
+    }
   }
 }
 
